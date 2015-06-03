53704ec13ee260e69eb97d29516c6a616803b92d
paigeful <mapeiqi.paige@gmail.com>
2015-06-03 11:03:17 -0700

    Update clockwise.js

--- js/clockwise.js
+++ js/clockwise.js
@@ -1,5 +1,3 @@
-// print out a 2D array in clockwise direction
-// ----------
 var arr1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 var arr2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]];
 var arr3 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
@@ -8,12 +6,13 @@ var arr5 = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14,15,16], [17, 18
 var arr6 = [[1], [2], [3]];
 var arr7 = [[1], [2]];
 
-function sortArray (arr) {
-  var rr = [];
+function sortArray (arr, dir) {
+  var result = [];
+  dir = dir || 'clockwise'; // default to clockwise
   function clockAll (arr) {
-    var newArr = [];
-    var m1 = [];
-    var m2 = [];
+    var middleArray = [];
+    var leftWall = [];
+    var rightWall = [];
     var n;
     var m;
     
@@ -29,40 +28,50 @@ function sortArray (arr) {
       
       if (m > 0) {
         for (var i = 1; i < n - 1; i++) {
-          m1.push(arr[i][m-1]);
+          leftWall.push(arr[i][m-1]);
           if (m > 1) {
-            m2.push(arr[i][0]);
+            rightWall.push(arr[i][0]);
           }
+          middleArray.push(arr[i].slice(1, arr[i].length - 1));
         }
       }
-      
-      rr.push(arr[0]);
-      if (m1.length > 0) {
-        rr.push(m1);
+      if (dir === 'clockwise') {
+        result.push(arr[0]);
+        if (leftWall.length > 0) {
+          result.push(leftWall);
+        }
+        if (n > 1) {
+          result.push(arr[n - 1].reverse());
+        }
+        if (rightWall.length > 0) {
+          result.push(rightWall.reverse());
+        }
+      } else if (dir === 'counterClockwise') {
+        result.push(arr[0][0]);
+        if (rightWall.length > 0) {
+          result.push(rightWall);
+        }
+        if (n > 1) {
+          result.push(arr[n - 1]);
+        }
+        if (leftWall.length > 0) {
+          result.push(leftWall.reverse());
+        }
+        if (m > 1) {
+          result.push(arr[0].slice(1).reverse());
+        }
       }
-      if (n > 1) {
-        rr.push(arr[n - 1].reverse());
-      }
-      if (m2.length > 0) {
-        rr.push(m2.reverse());
-      }
-      
-      arr = arr.slice(1, n - 1);
-      for (var key in arr) {
-        var arrayli = arr[key];
-        newArr.push(arrayli.slice(1, arrayli.length - 1));
-      }
-      clockAll(newArr);
+      clockAll(middleArray);
     }
     
   }
   clockAll(arr);
-  console.log(rr.toString());
+  console.log(result.toString());
 }
-sortArray(arr1);
+sortArray(arr1, 'counterClockwise');
 sortArray(arr2);
 sortArray(arr3);
-sortArray(arr4);
-sortArray(arr5);
-sortArray(arr6);
-sortArray(arr7);
+// sortArray(arr4);
+// sortArray(arr5);
+// sortArray(arr6);
+// sortArray(arr7);
