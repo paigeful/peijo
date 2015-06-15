df1436a22e29f8b2641d4831d7202b02114a740a
pma <pma@yahoo-inc.com>
2015-06-15 14:12:24 -0700

    determine draw

--- js/tic-tac-toe.js
+++ js/tic-tac-toe.js
@@ -8,25 +8,26 @@ Based on these conditions your program should never print out a full board unles
 var playerOneCount = 0;
 var playerTwoCount = 0;
 var drawCount = 0;
+var playerOne = 0;
 var baseBoard;
-var step;
-var row;
-var col;
 var count;
 var lastMove;
+var blankSpot;
 var finalWinner;
 var start;
 var end;
 
 start = new Date().getTime();
+
 // run program until one side win times
 while (playerOneCount < 10 && playerTwoCount < 10) {
     baseBoard = [[null, null, null],[null, null, null],[null, null, null]];
     count = 0;
     lastMove = [];
+    blankSpot = [];
     finalWinner = undefined;
 
-    for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < 8; i++) {
         // make a random move
         lastMove = markBoard();
 
@@ -35,59 +36,41 @@ while (playerOneCount < 10 && playerTwoCount < 10) {
             finalWinner = findWinner(baseBoard, lastMove);
         }
 
+        // one the 8th move, if there is no winner
+        // mimic the last move and determine if there will be a winner or a draw
+        if (i === 7 && finalWinner === undefined) {
+            blankSpot = locateBlankSpot(baseBoard);
+            if (findWinner(baseBoard, blankSpot, playerOne) === 0) {
+                finalWinner = 0;
+            } else {
+                finalWinner = 'draw';
+                baseBoard[blankSpot[0]][blankSpot[1]] = null;
+            }
+        }
+
         // if find winner, then print out result and break loop
         if (finalWinner !== undefined) {
             if (finalWinner === 0) {
-              playerOneCount++;
-            } else {
-              playerTwoCount++;
+                playerOneCount++;
+                console.log(getPlayer(finalWinner) + ' won. Board:');
+            } else if (finalWinner === 1) {
+                playerTwoCount++;
+                console.log(getPlayer(finalWinner) + ' won. Board:');
+            } else if (finalWinner === 'draw') {
+                drawCount++;
+                console.log('This will be a draw. Board:');
             }
-            console.log(getPlayer(finalWinner) + ' won. Board:');
+
             printBoard(baseBoard);
             console.log('------');
             break;
         }
-        // if last game still no winner, then it's a draw
-        if (i === 8) {
-            drawCount++;
-            console.log('This will be a draw. Board:');
-            printBoard(baseBoard);
-            console.log('------');
-        }
     }
 }
-
-// console.log("X:" + playerOneCount);
-// console.log("O:" + playerTwoCount);
-// console.log("drawCount:" + drawCount);
 
 console.log(playerOneCount === 10 ? "X won 10 games." : "O won 10 games.");
 end = new Date().getTime();
-console.log("time consumed:" + (end-start));
-
-
-
-
-function printBoard (board) {
-    for (var r = 0; r < 3; r++) {
-        console.log(getPlayer(board[r][0]) + '|' + getPlayer(board[r][1]) + '|' + getPlayer(board[r][2]));
-    }
-}
-
-function getPlayer (input) {
-    var output;
-    switch (input) {
-    case 0:
-        output = 'X';
-        break;
-    case 1:
-        output = 'O';
-        break;
-    default:
-        output = ' ';
-    }
-    return output;
-}
+console.log("time consumed:" + (end-start) + 'ms');
 
 function markBoard () {
     var step;
@@ -117,14 +100,48 @@ function makeRandomMove () {
     return [Math.floor(step / 3), step % 3];
 }
 
+function locateBlankSpot (board) {
+    for (var t = 0; t < 3; t++) {
+        if (baseBoard[t].indexOf(null) !== -1) {
+            return [t, baseBoard[t].indexOf(null)];
+        }
+    }
+    return;
+}
 
-function findWinner (board, lastMove) {
+function printBoard (board) {
+    for (var r = 0; r < 3; r++) {
+        console.log(getPlayer(board[r][0]) + '|' + getPlayer(board[r][1]) + '|' + getPlayer(board[r][2]));
+    }
+}
+
+function getPlayer (input) {
+    var output;
+    switch (input) {
+    case 0:
+        output = 'X';
+        break;
+    case 1:
+        output = 'O';
+        break;
+    default:
+        output = ' ';
+    }
+    return output;
+}
+
+function findWinner (board, lastMove, player) {
     if (!lastMove) {
         return;
     }
 
     var row = lastMove[0];
     var col = lastMove[1];
+
+    // mimic player move
+    if (player === 0 || player === 1) {
+        board[row][col] = player;
+    }
 
     // test the related row and col
     if (board[row][0] === board[row][1] && board[row][0] === board[row][2]) {
